ffa086433e4ba33a35b381118a90e6be1d648886
abond32 <abond32@gmail.com>
2023-12-29 23:05:53 -0500

    updated

--- shells_play/create_dynamic_files/create_dynamic_files.sh
+++ shells_play/create_dynamic_files/create_dynamic_files.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
+#!/bin/bash                                            
 
-# The directory where the files are located
 DIRECTORY=$1
+MESSAGE=$2                                       
+FILE_NAME=$3                                   
+FILE_TYPE=$4                                   
 
-# The message to append
-MESSAGE=$2
-
-# This is the file type
-FILE_TYPE=$3
-
-# Change to the directory
 cd "$DIRECTORY" || exit
 
+for i in {1..5}; do
+  echo "$MESSAGE" | tee -a "$FILE_NAME"_"$i"."$FILE_TYPE" > /dev/null
+  done
+
 # Append the message to each file
-for i in {1..2}; do
-  echo "$MESSAGE" | tee -a vars_*"$i".txt tmp_*"$i".txt > /dev/null
-done
+# loop in the range of 1 to 5
+# echo the message and pipe it to the tee command
+# tee the output to the file 
+# -a append the output to the file instead of overwriting it
+# > /dev/null redirect the output to the null device (discard it) no display on the terminal
+
